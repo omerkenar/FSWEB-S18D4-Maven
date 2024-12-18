67390568cbcd39affa3e3dca35f82b955337e6bb
Ömer Kenar <omer.ken@hotmail.com>
2024-12-18 23:57:45 +0300

    odev tamam

---
+++ src/main/java/com/workintech/s18d1/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.workintech.s18d1.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    private ResponseEntity<BurgerErrorResponse> handleException(BurgerException exception){
+        log.error("burger exception accured.. Exception details: ",exception);
+
+        BurgerErrorResponse burgerErrorResponse = new BurgerErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(burgerErrorResponse, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<BurgerErrorResponse> handleException(Exception exception){
+        log.error("exception accured.. Exception details: ",exception);
+
+        BurgerErrorResponse burgerErrorResponse = new BurgerErrorResponse(exception.getMessage());
+
+        return new ResponseEntity<>(burgerErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
